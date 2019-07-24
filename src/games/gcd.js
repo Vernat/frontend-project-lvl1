b06450042d460b90e643ccdd7f982e362bc43d0c
Vernat Khisamov <v.khisamov@petrosoftinc.com>
2019-07-24 09:09:38 +0500

    Move gcd function out or round function

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,19 +5,20 @@ export const showBrainGcdRulesInfo = () => {
   console.log('Find the greatest common divisor of given numbers.\n');
 };
 
-export const brainGcdRound = () => {
-  const getGcd = (val1, val2) => {
-    let a = val1;
-    let b = val2;
-    while (a !== 0 && b !== 0) {
-      if (a > b) {
-        a %= b;
-      } else {
-        b %= a;
-      }
+const getGcd = (val1, val2) => {
+  let a = val1;
+  let b = val2;
+  while (a !== 0 && b !== 0) {
+    if (a > b) {
+      a %= b;
+    } else {
+      b %= a;
     }
-    return a + b;
-  };
+  }
+  return a + b;
+};
+
+export const brainGcdRound = () => {
   const maxValue = 100;
   const a = getRandomInt(maxValue);
   const b = getRandomInt(maxValue);
