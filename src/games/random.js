c9ad48be4fc20ad32bffbb1159286e66d5879f43
Vernat Khisamov <v.khisamov@petrosoftinc.com>
2019-07-24 12:22:46 +0500

    remove unused function

--- src/games/random.js
+++ src/games/random.js
@@ -3,4 +3,3 @@ export const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
 export const add = (a, b) => a + b;
 export const minus = (a, b) => a - b;
-export const mul = (a, b) => a * b;
