ea913ff8f760d2c4687e1d5bd18adb3a70eb5256
Vernat Khisamov <v.khisamov@petrosoftinc.com>
2019-07-30 12:35:00 +0500

    Refactor getting random function in the calc game

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,5 +1,4 @@
-import { cons } from '@hexlet/pairs';
-import { getRandomInt } from './random';
+import { getRandomInt, getRandomOperationWithAnswer } from './random';
 import { runBrainGame } from './engine';
 
 export const getRulesInfo = () => 'What is the result of the expression?\n';
@@ -8,13 +7,7 @@ export const brainCalcRound = () => {
   const maxValue = 30;
   const a = getRandomInt(maxValue);
   const b = getRandomInt(maxValue);
-  const randOperationValue = getRandomInt(maxValue) % 3;
-  switch (randOperationValue) {
-    case 0: return cons(`${a} + ${b}`, String(a + b));
-    case 1: return cons(`${a} - ${b}`, String(a - b));
-    case 2: return cons(`${a} * ${b}`, String(a * b));
-    default: return cons(`${a} * ${b}`, String(a * b));
-  }
+  return getRandomOperationWithAnswer(a, b);
 };
 
 export default () => runBrainGame(brainCalcRound, getRulesInfo);
--- src/games/random.js
+++ src/games/random.js
@@ -1,5 +1,20 @@
+import { cons } from '@hexlet/pairs';
 
 export const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
 export const add = (a, b) => a + b;
 export const minus = (a, b) => a - b;
+export const multiply = (a, b) => a * b;
+
+export const getStringForAdd = (a, b) => `${a} + ${b}`;
+export const getStringForMinus = (a, b) => `${a} - ${b}`;
+export const getStringForMultiply = (a, b) => `${a} * ${b}`;
+
+export const getPairForAdd = (a, b) => cons(getStringForAdd(a, b), `${add(a, b)}`);
+export const getPairForMinus = (a, b) => cons(getStringForMinus(a, b), `${minus(a, b)}`);
+export const getPairForMultiply = (a, b) => cons(getStringForMultiply(a, b), `${multiply(a, b)}`);
+
+export const getRandomOperationWithAnswer = (a, b) => {
+  const variants = [getPairForAdd, getPairForMinus, getPairForMultiply];
+  return variants[getRandomInt(variants.length)](a, b);
+};
