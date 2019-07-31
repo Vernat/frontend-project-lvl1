d9778e73b9947cff3f4f0df450e6170668eca6b4
Vernat Khisamov <v.khisamov@petrosoftinc.com>
2019-07-31 11:12:08 +0500

    move specific calc functions

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,7 +1,19 @@
-import { getRandomNumberBetween, getRandomOperationWithAnswer } from './random';
+import { cons } from '@hexlet/pairs';
+import {
+  getRandomNumberBetween, add, minus, multiply,
+} from './random';
 import { runBrainGame } from './engine';
 
 export const getRulesInfo = () => 'What is the result of the expression?\n';
+
+const getRoundDataForAdd = (a, b) => cons(`${a} + ${b}`, `${add(a, b)}`);
+const getRoundDataForMinus = (a, b) => cons(`${a} - ${b}`, `${minus(a, b)}`);
+const getRoundDataForMultiply = (a, b) => cons(`${a} * ${b}`, `${multiply(a, b)}`);
+
+const getRandomOperationWithAnswer = (a, b) => {
+  const roundVariants = [getRoundDataForAdd, getRoundDataForMinus, getRoundDataForMultiply];
+  return roundVariants[getRandomNumberBetween(0, roundVariants.length)](a, b);
+};
 
 export const getNextRound = () => {
   const maxValue = 30;
--- src/games/random.js
+++ src/games/random.js
@@ -1,20 +1,5 @@
-import { cons } from '@hexlet/pairs';
-
-export const getRandomNumberBetween = (min, max) => Math.random() * (max - min) + min;
+export const getRandomNumberBetween = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 export const add = (a, b) => a + b;
 export const minus = (a, b) => a - b;
 export const multiply = (a, b) => a * b;
-
-export const getStringForAdd = (a, b) => `${a} + ${b}`;
-export const getStringForMinus = (a, b) => `${a} - ${b}`;
-export const getStringForMultiply = (a, b) => `${a} * ${b}`;
-
-export const getPairForAdd = (a, b) => cons(getStringForAdd(a, b), `${add(a, b)}`);
-export const getPairForMinus = (a, b) => cons(getStringForMinus(a, b), `${minus(a, b)}`);
-export const getPairForMultiply = (a, b) => cons(getStringForMultiply(a, b), `${multiply(a, b)}`);
-
-export const getRandomOperationWithAnswer = (a, b) => {
-  const variants = [getPairForAdd, getPairForMinus, getPairForMultiply];
-  return variants[getRandomNumberBetween(0, variants.length)](a, b);
-};
