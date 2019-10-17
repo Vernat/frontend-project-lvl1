e19ada6534f12642bbbb530a9d4d9c48cc2dd9ba
Vernat Khisamov <v.khisamov@petrosoftinc.com>
2019-10-17 12:38:50 +0300

    remove unnecessary code

--- src/bin/brain-games.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!\n');
-const userName = readlineSync.question('May I have your name?\n');
-console.log(`Hello ${userName}\n`);
