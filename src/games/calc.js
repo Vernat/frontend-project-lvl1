a1a2fdd491843a13bf8b7fa7099ff04d4e628edc
Vernat Khisamov <v.khisamov@petrosoftinc.com>
2019-10-27 02:14:24 +0300

    optimize calc to simply the adding of new operations

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,4 +1,4 @@
-import { cons } from '@hexlet/pairs';
+import { cons, car, cdr } from '@hexlet/pairs';
 import { l, get, length } from '@hexlet/pairs-data';
 import getRandomNumberBetween from '../random';
 import { runBrainGame } from '../engine';
@@ -6,23 +6,22 @@ import { runBrainGame } from '../engine';
 const add = (a, b) => a + b;
 const minus = (a, b) => a - b;
 const multiply = (a, b) => a * b;
+const operationInfoList = l(cons('+', add), cons('-', minus), cons('*', multiply));
+const getRandomOperation = () => get(
+  getRandomNumberBetween(0, length(operationInfoList)),
+  operationInfoList,
+);
+const getOperationSymbol = operation => car(operation);
+const getOperationFunc = operation => cdr(operation);
 
-const getRoundDataForAdd = (a, b) => cons(`${a} + ${b}`, `${add(a, b)}`);
-const getRoundDataForMinus = (a, b) => cons(`${a} - ${b}`, `${minus(a, b)}`);
-const getRoundDataForMultiply = (a, b) => cons(`${a} * ${b}`, `${multiply(a, b)}`);
-const roundVariants = l(getRoundDataForAdd, getRoundDataForMinus, getRoundDataForMultiply);
-
-const getRandomOperationWithAnswer = (a, b) => get(
-  getRandomNumberBetween(0, length(roundVariants)),
-  roundVariants,
-)(a, b);
-
-const getNextRound = () => {
 const getRoundData = () => {
   const maxValue = 30;
   const a = getRandomNumberBetween(0, maxValue);
   const b = getRandomNumberBetween(0, maxValue);
-  return getRandomOperationWithAnswer(a, b);
+  const operation = getRandomOperation();
+  const operationSymbol = getOperationSymbol(operation);
+  const operationFunc = getOperationFunc(operation);
+  return cons(`${a} ${operationSymbol} ${b}`, `${operationFunc(a, b)}`);
 };
 
 const gameInfo = 'What is the result of the expression?\n';
