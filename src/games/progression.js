ec30f79eeaf3f3c6fcd817f022cca110435e397d
Vernat Khisamov <v.khisamov@petrosoftinc.com>
2019-11-11 00:16:09 +0300

    make getQuestion pure and move to a module level

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,22 +6,21 @@ import { runBrainGame } from '../engine';
 const progressionLength = 10;
 const maxRandomValue = 20;
 
+const getQuestion = (begin, iterationCounter, maxLength, hiddenIndex, step) => {
+  if (iterationCounter === maxLength) {
+    return '';
+  }
+  const next = begin + step * (iterationCounter);
+  const questionPart = hiddenIndex === iterationCounter ? '..' : `${next}`;
+  return `${questionPart} ${getQuestion(begin, iterationCounter + 1, maxLength, hiddenIndex, step)}`;
+};
+
 const getRoundData = () => {
   const start = getRandomNumber(0, maxRandomValue);
   const hiddenElementIndex = getRandomNumber(0, progressionLength);
-  const step = getRandomNumber(-maxRandomValue, maxRandomValue);
-
-  const getQuestion = (begin, iterationCounter) => {
-    if (iterationCounter === progressionLength) {
-      return '';
-    }
-    const questionPart = hiddenElementIndex === iterationCounter ? '..' : `${begin}`;
-    const next = start + step * (iterationCounter + 1);
-    return `${questionPart} ${getQuestion(next, iterationCounter + 1)}`;
-  };
-
-  const question = getQuestion(start, 0);
-  const correctAnswer = (start + step * hiddenElementIndex).toString();
+  const stepSize = getRandomNumber(-maxRandomValue, maxRandomValue);
+  const question = getQuestion(start, 0, progressionLength, hiddenElementIndex, stepSize);
+  const correctAnswer = (start + stepSize * hiddenElementIndex).toString();
   return cons(question, correctAnswer);
 };
 
