3a2a8eb2449c805d3506181e9ce2c36293ce1aba
Vernat Khisamov <v.khisamov@petrosoftinc.com>
2019-11-18 00:16:33 +0300

    decrease params number in getQuestion

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,20 +6,23 @@ import { runBrainGame } from '../engine';
 const progressionLength = 10;
 const maxRandomValue = 20;
 
-const getQuestion = (begin, iterationCounter, maxLength, hiddenIndex, step) => {
-  if (iterationCounter === maxLength) {
-    return '';
-  }
-  const next = begin + step * (iterationCounter);
-  const questionPart = hiddenIndex === iterationCounter ? '..' : `${next}`;
-  return `${questionPart} ${getQuestion(begin, iterationCounter + 1, maxLength, hiddenIndex, step)}`;
+const getQuestion = (start, stepSize, lengthOfProgression, hiddenElementIndex) => {
+  const iteration = (iterationCounter) => {
+    if (iterationCounter === lengthOfProgression) {
+      return '';
+    }
+    const next = start + stepSize * (iterationCounter);
+    const questionPart = hiddenElementIndex === iterationCounter ? '..' : `${next}`;
+    return `${questionPart} ${iteration(iterationCounter + 1)}`;
+  };
+  return iteration(0);
 };
 
 const getRoundData = () => {
   const start = getRandomNumber(0, maxRandomValue);
   const hiddenElementIndex = getRandomNumber(0, progressionLength);
   const stepSize = getRandomNumber(-maxRandomValue, maxRandomValue);
-  const question = getQuestion(start, 0, progressionLength, hiddenElementIndex, stepSize);
+  const question = getQuestion(start, stepSize, progressionLength, hiddenElementIndex);
   const correctAnswer = (start + stepSize * hiddenElementIndex).toString();
   return cons(question, correctAnswer);
 };
